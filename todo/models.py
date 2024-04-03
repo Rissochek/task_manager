38ec0919d876d401802d67c2fa602131ace786e7
velievrauf <veliev.rauf07@gmail.com>
2024-04-03 22:04:23 +0300

    DataBase

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,36 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Time, create_engine
 
-db = SQLAlchemy()
+DATABASE_NAME = 'app.db'
+
+engine = create_engine(f'sqlite:///{DATABASE_NAME}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
 
 
-class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    is_complete = db.Column(db.Boolean)
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Tasks(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    status = Column(Boolean)
+    deadline = Column(Time)
+    priority = Column(Integer)
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+
+create_db()
