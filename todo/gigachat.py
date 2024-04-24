c921b49510dc61418facec477caf007dea5994e6
velievrauf <veliev.rauf07@gmail.com>
2024-04-24 20:31:37 +0300

    Убраны комментарии + PEP8

--- todo/gigachat.py
+++ todo/gigachat.py
@@ -4,19 +4,17 @@ import base64
 import uuid
 import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # Игнорирование предупреждения "InsecureRequestWarning"
-
-client_id = ""  # индивидуально
-secret = ""  # индивидуально
-auth = ""  # индивидуально
+client_id = ""
+secret = ""
+auth = ""
 
 credentials = f"{client_id}:{secret}"
 encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
 
 
 def get_token(auth_token, scope='GIGACHAT_API_PERS'):
-    # Создание идентификатора UUID (36 знаков)
     rq_uid = str(uuid.uuid4())
 
     url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
@@ -28,7 +26,6 @@ def get_token(auth_token, scope='GIGACHAT_API_PERS'):
         'Authorization': f'Basic {auth_token}'
     }
 
-    # Тело запроса
     payload = {
         'scope': scope
     }
@@ -41,7 +38,7 @@ def get_token(auth_token, scope='GIGACHAT_API_PERS'):
         return -1
 
 
-response = get_token(auth)  # Полученный токен доступа
+response = get_token(auth)
 if response != 1:
     giga_token = response.json()['access_token']
 
@@ -49,31 +46,29 @@ if response != 1:
 def get_chat_completion(auth_token, user_message):
     url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
 
-    # Подготовка данных запроса в формате JSON
     payload = json.dumps({
-        "model": "GigaChat-Pro",  # Используемая модель
+        "model": "GigaChat-Pro",
         "messages": [
             {
-                "role": "user",  # Роль отправителя (пользователь)
-                "content": user_message  # Содержание сообщения
+                "role": "user",
+                "content": user_message
             }
         ],
-        "temperature": 1,  # Температура генерации
-        "top_p": 0.1,  # Параметр top_p для контроля разнообразия ответов
-        "n": 1,  # Количество возвращаемых ответов
-        "stream": False,  # Потоковая ли передача ответов
-        "max_tokens": 512,  # Максимальное количество токенов в ответе
-        "repetition_penalty": 1,  # Штраф за повторения
-        "update_interval": 0  # Интервал обновления (для потоковой передачи)
+        "temperature": 1,
+        "top_p": 0.1,
+        "n": 1,
+        "stream": False,
+        "max_tokens": 512,
+        "repetition_penalty": 1,
+        "update_interval": 0
     })
 
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': f'Bearer {auth_token}'  # Токен авторизации
+        'Authorization': f'Bearer {auth_token}'
     }
 
-    # Выполнение POST-запроса и возвращение ответа
     try:
         response = requests.request("POST", url, headers=headers, data=payload, verify=False)
         return response
@@ -82,6 +77,6 @@ def get_chat_completion(auth_token, user_message):
         return -1
 
 
-answer = get_chat_completion(giga_token, user_message='')  # текст GigaChat от пользователя
+answer = get_chat_completion(giga_token, user_message='')
 answer.json()
 print(answer.json()['choices'][0]['message']['content'])
