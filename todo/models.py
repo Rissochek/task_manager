8ebd0f709066f990fe0ee7edd7902216a45bd3e5
Ruslan <ruslanynysov@mail.ru>
2024-05-01 22:30:33 +0300

    Исправил аннотацию типов под формат mypy

--- todo/models.py
+++ todo/models.py
@@ -1,13 +1,16 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, create_engine
+from typing import Any
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker
+from sqlalchemy import Column, ForeignKey, create_engine, Integer, String, Boolean, DateTime
 from flask_sqlalchemy import SQLAlchemy
 
-DATABASE_NAME = 'app.db'
+DATABASE_NAME: str = 'app.db'
 
 engine = create_engine(f'sqlite:///{DATABASE_NAME}')
-Session = sessionmaker(bind=engine)
+Session: sessionmaker = sessionmaker(bind=engine)
 
-db = SQLAlchemy()
+db: Any = SQLAlchemy()
 
 
 class Category(db.Model):
@@ -22,10 +25,10 @@ class Category(db.Model):
     """
     __tablename__ = 'category'
 
-    id: int = Column(Integer, primary_key=True)
-    name: str = Column(String)
-    sorting: str = Column(String)
-    filtering: str = Column(String, default='skip')
+    id: Column[int] = Column(Integer, primary_key=True)
+    name: Column[str] = Column(String)
+    sorting: Column[str] = Column(String)
+    filtering: Column[str] = Column(String, default='skip')
 
 
 class Tasks(db.Model):
@@ -43,10 +46,10 @@ class Tasks(db.Model):
     """
     __tablename__ = 'tasks'
 
-    id: int = Column(Integer, primary_key=True)
-    title: str = Column(String)
-    description: str = Column(String)
-    status: bool = Column(Boolean)
-    deadline: DateTime = Column(DateTime)
-    priority: int = Column(Integer, default=6)
-    category_id: int = Column(Integer, ForeignKey('category.id'))
+    id: Column[int] = Column(Integer, primary_key=True)
+    title: Column[str] = Column(String)
+    description: Mapped[str] = mapped_column(default='')
+    status: Column[bool] = Column(Boolean)
+    deadline: Mapped[datetime] = mapped_column(default=datetime.now())
+    priority: Column[int] = Column(Integer, default=6)
+    category_id: Column[int] = Column(Integer, ForeignKey('category.id'))
