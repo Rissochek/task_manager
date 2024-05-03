a441f161b243cb5f66854a2890806a54b5b13358
Rissochek <146011723+Rissochek@users.noreply.github.com>
2024-05-03 21:42:18 +0300

    Update app.py
    Убрал режим debug

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from todo.routes import db, app
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True, port=app.config['PORT'])
+        app.run(port=app.config['PORT'])
