3955d45c9d491a2bb873c6108ee274c13c0e51ab
EgorX2000 <76452046+EgorX2000@users.noreply.github.com>
2024-05-06 19:59:47 +0300

    comments script.js

--- todo/static/js/script.js
+++ todo/static/js/script.js
@@ -1,4 +1,4 @@
-
+// calendar
 $(document).ready(function () {
     $('[id^="example-"]').each(function () {
         var taskId = $(this).data('task-id');
@@ -16,6 +16,7 @@ $(document).ready(function () {
 });
 $('.ui.dropdown').dropdown();
 
+// edit task
 document.addEventListener("DOMContentLoaded", function () {
     var editIcons = document.querySelectorAll(".edit_task");
 
@@ -42,8 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-
+// edit category
 document.addEventListener("DOMContentLoaded", function () {
     var editIcons = document.querySelectorAll(".edit_category");
 
@@ -70,61 +70,48 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
+// modal window
 let isModalOpen = false;
 
-// Обработчик события открытия модального окна
 function openModal() {
     isModalOpen = true;
 }
 
-// Обработчик события закрытия модального окна
 function closeModal() {
     isModalOpen = false;
 }
 
-// Проверка состояния модального окна перед обновлением страницы
 window.addEventListener('beforeunload', function (event) {
     if (isModalOpen) {
-        // Отмена события закрытия окна
         event.preventDefault();
-        // Возвращение текста, который будет отображаться в диалоговом окне браузера
         event.returnValue = '';
     }
 });
 
 
-// Найти все заголовки задач и добавить обработчик клика
 var taskHeaders = document.querySelectorAll('.open-modal');
 taskHeaders.forEach(function (header) {
     header.addEventListener('click', function () {
-        // Найти id задачи из data-атрибута
         var taskId = header.getAttribute('data-task-id');
-        // Показать модальное окно для этой задачи
         var modal = document.getElementById('description-' + taskId);
         modal.style.display = "block";
-        // Сохранить ID открытого модального окна в Local Storage
         localStorage.setItem('openModalId', taskId);
     });
 });
 
-// Проверить, было ли сохранено ID открытого модального окна в Local Storage
 var openModalId = localStorage.getItem('openModalId');
 if (openModalId) {
-    // Показать сохраненное модальное окно
     var modal = document.getElementById('description-' + openModalId);
     if (modal) {
         modal.style.display = "block";
     }
 }
 
-// Закрыть модальное окно при клике вне его области
 window.addEventListener('click', function (event) {
     var modals = document.querySelectorAll('.modal');
     modals.forEach(function (modal) {
         if (event.target == modal) {
             modal.style.display = "none";
-            // Удалить ID открытого модального окна из Local Storage при закрытии модального окна
             localStorage.removeItem('openModalId');
         }
     });
